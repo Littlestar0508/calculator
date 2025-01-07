85d22ff9307f93fb464c31fc71d83854f7f32100
Littlestar0508 <rist9860@gmail.com>
2025-01-07 17:08:14 +0900

    숫자 클릭 시 연산 식에 숫자를 추가하는 이벤트 생성

--- calc.js
+++ calc.js
@@ -1,1 +1,44 @@
-console.log(1);
+var buttons = document.querySelectorAll("button");
+var operation = document.querySelector(".operation");
+var output = document.querySelector(".output");
+var eventButtons = Array.from(buttons);
+eventButtons.map(function (idx) {
+    idx.addEventListener("click", function (e) {
+        var target = e.target;
+        var targetText = target.textContent;
+        if (!isNaN(Number(targetText))) {
+            switch (Number(targetText)) {
+                case 1:
+                    operation === null || operation === void 0 ? void 0 : operation.textContent += 1;
+                    break;
+                case 2:
+                    operation === null || operation === void 0 ? void 0 : operation.textContent += 2;
+                    break;
+                case 3:
+                    operation === null || operation === void 0 ? void 0 : operation.textContent += 3;
+                    break;
+                case 4:
+                    operation === null || operation === void 0 ? void 0 : operation.textContent += 4;
+                    break;
+                case 5:
+                    operation === null || operation === void 0 ? void 0 : operation.textContent += 5;
+                    break;
+                case 6:
+                    operation === null || operation === void 0 ? void 0 : operation.textContent += 6;
+                    break;
+                case 7:
+                    operation === null || operation === void 0 ? void 0 : operation.textContent += 7;
+                    break;
+                case 8:
+                    operation === null || operation === void 0 ? void 0 : operation.textContent += 8;
+                    break;
+                case 9:
+                    operation === null || operation === void 0 ? void 0 : operation.textContent += 9;
+                    break;
+                case 0:
+                    operation === null || operation === void 0 ? void 0 : operation.textContent += 0;
+                    break;
+            }
+        }
+    });
+});
--- calc.ts
+++ calc.ts
@@ -1,1 +1,47 @@
-console.log(1);
+let buttons = document.querySelectorAll("button") as NodeListOf<HTMLButtonElement>;
+const operation = document.querySelector(".operation") as HTMLSpanElement;
+const output = document.querySelector(".output") as HTMLOutputElement;
+
+let eventButtons: Array<HTMLButtonElement> = Array.from(buttons);
+
+eventButtons.map((idx: HTMLButtonElement) => {
+  idx.addEventListener("click", (e: MouseEvent) => {
+    const target = e.target as HTMLButtonElement;
+    const targetText = target.textContent;
+
+    if (!isNaN(Number(targetText))) {
+      switch (Number(targetText)) {
+        case 1:
+          operation?.textContent += 1;
+          break;
+        case 2:
+          operation?.textContent += 2;
+          break;
+        case 3:
+          operation?.textContent += 3;
+          break;
+        case 4:
+          operation?.textContent += 4;
+          break;
+        case 5:
+          operation?.textContent += 5;
+          break;
+        case 6:
+          operation?.textContent += 6;
+          break;
+        case 7:
+          operation?.textContent += 7;
+          break;
+        case 8:
+          operation?.textContent += 8;
+          break;
+        case 9:
+          operation?.textContent += 9;
+          break;
+        case 0:
+          operation?.textContent += 0;
+          break;
+      }
+    }
+  });
+});
