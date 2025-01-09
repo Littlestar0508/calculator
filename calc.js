27710c222aede0a0620e2343fce7880a66d69b46
Littlestar0508 <rist9860@gmail.com>
2025-01-10 01:25:43 +0900

    계산기 숫자 렌더링 및 초기화 구현

--- calc.js
+++ calc.js
@@ -1,44 +1,52 @@
 var buttons = document.querySelectorAll("button");
 var operation = document.querySelector(".operation");
 var output = document.querySelector(".output");
+var resetButton = document.querySelector(".ac");
 var eventButtons = Array.from(buttons);
+var calc = document.querySelectorAll(".opt");
+var calcSet = Array.from(calc);
+var result = 100;
+var cur = "";
+var arr = [];
 eventButtons.map(function (idx) {
-    idx.addEventListener("click", function (e) {
-        var target = e.target;
-        var targetText = target.textContent;
-        if (!isNaN(Number(targetText))) {
-            switch (Number(targetText)) {
-                case 1:
-                    operation === null || operation === void 0 ? void 0 : operation.textContent += 1;
-                    break;
-                case 2:
-                    operation === null || operation === void 0 ? void 0 : operation.textContent += 2;
-                    break;
-                case 3:
-                    operation === null || operation === void 0 ? void 0 : operation.textContent += 3;
-                    break;
-                case 4:
-                    operation === null || operation === void 0 ? void 0 : operation.textContent += 4;
-                    break;
-                case 5:
-                    operation === null || operation === void 0 ? void 0 : operation.textContent += 5;
-                    break;
-                case 6:
-                    operation === null || operation === void 0 ? void 0 : operation.textContent += 6;
-                    break;
-                case 7:
-                    operation === null || operation === void 0 ? void 0 : operation.textContent += 7;
-                    break;
-                case 8:
-                    operation === null || operation === void 0 ? void 0 : operation.textContent += 8;
-                    break;
-                case 9:
-                    operation === null || operation === void 0 ? void 0 : operation.textContent += 9;
-                    break;
-                case 0:
-                    operation === null || operation === void 0 ? void 0 : operation.textContent += 0;
-                    break;
-            }
-        }
-    });
+    idx.addEventListener("click", insertNumber);
 });
+resetButton.addEventListener("click", resetAll);
+calcSet.map(function (idx) {
+    idx.addEventListener("click", calcNumber);
+});
+function resetAll() {
+    operation.textContent = "0";
+    output.textContent = "0";
+    result = 0;
+}
+function insertNumber(e) {
+    var target = e.target;
+    if (target.dataset.num) {
+        changeInputNumber(target.dataset.num);
+    }
+}
+function changeInputNumber(newInput) {
+    if (operation.textContent === "0") {
+        operation.textContent = newInput;
+    }
+    else {
+        operation.textContent += newInput;
+    }
+    cur += newInput;
+    console.log(cur);
+}
+function calcNumber(e) {
+    var title = e.target.title;
+    switch (title) {
+        case "양수, 음수 전환":
+            console.log(1);
+            break;
+        case "계산":
+            output.textContent = String(result);
+    }
+}
+function chkBeforeCalc() {
+    if (arr.length !== 0) {
+    }
+}
--- calc.ts
+++ calc.ts
@@ -1,47 +1,60 @@
-let buttons = document.querySelectorAll("button") as NodeListOf<HTMLButtonElement>;
+const buttons = document.querySelectorAll("button") as NodeListOf<HTMLButtonElement>;
 const operation = document.querySelector(".operation") as HTMLSpanElement;
 const output = document.querySelector(".output") as HTMLOutputElement;
-
-let eventButtons: Array<HTMLButtonElement> = Array.from(buttons);
+const resetButton = document.querySelector(".ac") as HTMLButtonElement;
+const eventButtons: Array<HTMLButtonElement> = Array.from(buttons);
+const calc = document.querySelectorAll(".opt") as NodeListOf<HTMLButtonElement>;
+const calcSet: Array<HTMLButtonElement> = Array.from(calc);
+let result = 100;
+let cur: string = "";
+const arr: string[] = [];
 
 eventButtons.map((idx: HTMLButtonElement) => {
-  idx.addEventListener("click", (e: MouseEvent) => {
-    const target = e.target as HTMLButtonElement;
-    const targetText = target.textContent;
-
-    if (!isNaN(Number(targetText))) {
-      switch (Number(targetText)) {
-        case 1:
-          operation?.textContent += 1;
-          break;
-        case 2:
-          operation?.textContent += 2;
-          break;
-        case 3:
-          operation?.textContent += 3;
-          break;
-        case 4:
-          operation?.textContent += 4;
-          break;
-        case 5:
-          operation?.textContent += 5;
-          break;
-        case 6:
-          operation?.textContent += 6;
-          break;
-        case 7:
-          operation?.textContent += 7;
-          break;
-        case 8:
-          operation?.textContent += 8;
-          break;
-        case 9:
-          operation?.textContent += 9;
-          break;
-        case 0:
-          operation?.textContent += 0;
-          break;
-      }
-    }
-  });
+  idx.addEventListener("click", insertNumber);
 });
+
+resetButton.addEventListener("click", resetAll);
+calcSet.map((idx: HTMLButtonElement) => {
+  idx.addEventListener("click", calcNumber);
+});
+
+function resetAll() {
+  operation.textContent = "0";
+  output.textContent = "0";
+  result = 0;
+}
+
+function insertNumber(e: MouseEvent) {
+  const target = e.target as HTMLButtonElement;
+
+  if (target.dataset.num) {
+    changeInputNumber(target.dataset.num);
+  }
+}
+
+function changeInputNumber(newInput: string) {
+  if (operation.textContent === "0") {
+    operation.textContent = newInput;
+  } else {
+    operation.textContent += newInput;
+  }
+  cur += newInput;
+  console.log(cur);
+}
+
+function calcNumber(e: MouseEvent) {
+  const title: string = (e.target as HTMLButtonElement).title;
+
+  switch (title) {
+    case "양수, 음수 전환":
+      console.log(1);
+      break;
+    case "계산":
+      output.textContent = String(result);
+  }
+}
+
+function chkBeforeCalc() {
+  if (arr.length !== 0) {
+  }
+}
